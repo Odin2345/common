b18da8d6fbaeedb6a26b448961a18b8455c6543a
Odin2345 <odin2345@icloud.com>
2019-10-28 20:18:39 +0200

    Changes added

--- flask_lesson_intro/main.py
+++ flask_lesson_intro/main.py
@@ -6,42 +6,42 @@ app = Flask(__name__)
 
 @app.route('/')
 def get_home_page():
-    return render_template("home.html", data=get_data())
+    return render_template("home.html", data=get_data(), title="Home")
 
 
 @app.route('/author')
 def get_author_page():
-    return render_template("author.html", data=get_data())
+    return render_template("author.html", data=get_data(), title="Author")
 
 
 @app.route('/alarm_clock')
 def get_alarm_clock_page():
-    return render_template("alarm_clock.html", data=get_data())
+    return render_template("product.html", data=get_data(), title="Alarm clock")
 
 
 @app.route('/battery_charger')
 def get_battery_charger_page():
-    return render_template("battery_charger.html", data=get_data())
+    return render_template("product.html", data=get_data(), title="Battery charger")
 
 
 @app.route('/calculator')
 def get_calculator_page():
-    return render_template("calculator.html", data=get_data())
+    return render_template("product.html", data=get_data(), title="Calculator")
 
 
 @app.route('/coffee_maker')
 def get_coffee_maker_page():
-    return render_template("coffee_maker.html", data=get_data())
+    return render_template("product.html", data=get_data(), title="Coffeemaker")
 
 
 @app.route('/headphones')
 def get_headphones_page():
-    return render_template("headphones.html", data=get_data())
+    return render_template("product.html", data=get_data(), title="Headphones")
 
 
 @app.route('/ipod')
 def get_ipod_page():
-    return render_template("ipod.html", data=get_data())
+    return render_template("product.html", data=get_data(), title="iPod")
 
 
 if __name__ == "__main__":
